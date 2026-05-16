f529de1c361dc3d493c3170c16b99c6ba1a9f6f0
LUIZ RIBEIRO <luiz.ribeiro2@aluno.cps.sp.gov.br>
2026-05-15 23:00:10 -0300

    ##

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,9 +1,16 @@
 namespace MauiAppHotel.Views;
 
+using MauiAppHotel.Views;
+
 public partial class ContratacaoHospedagem : ContentPage
 {
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
 	}
+
+    private async void BtnSobre_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Sobre());
+    }
 }
