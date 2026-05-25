427609f3e33bef5a2f5f77293a79a3f8563afe19
LUIZ RIBEIRO <luiz.ribeiro2@aluno.cps.sp.gov.br>
2026-05-25 09:13:53 -0300

    #

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,7 +1,42 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Presidencial",
+                ValorDiaAdulto = 1200.0,
+                ValorDiaCrianca = 840.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Master",
+                ValorDiaAdulto = 1000.0,
+                ValorDiaCrianca = 700.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Standard",
+                ValorDiaAdulto = 800.0,
+                ValorDiaCrianca = 560.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Economic",
+                ValorDiaAdulto = 600.0,
+                ValorDiaCrianca = 420.0
+            },
+
+
+
+        };
         public App()
         {
             InitializeComponent();
---
+++ MauiAppHotel/Models/Quarto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiAppHotel.Models
+{
+    public class Quarto
+    {
+        public string Descricao { get; set; }
+        public double ValorDiaAdulto { get; set; }
+        public double ValorDiaCrianca { get; set; }
+    }
+}
